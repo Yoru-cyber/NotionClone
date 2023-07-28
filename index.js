bd1416c7f83b6e44e0b3b169a43026d27fcd60d6
Yoru-cyber <carlosmendez170210@gmail.com>
2023-07-28 17:56:39 -0400

    agregado client side y configuracion db

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "notes",
+});
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Conectado");
+});
 
-app.get("/", (req, res) => {
-  res.send("Hola mundo");
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.get("/listas", (req, res) => {
+  connection.query("SELECT * FROM listas", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+     return result;
+  });
+});
+
+app.get("/apiResponseTest", (req, res) => {
+  res.json({ message: "Hola desde el servidor" });
+  res.end();
 });
 
 app.listen(port, () => {
